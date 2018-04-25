dac82a968e18b5f1c32b2cf18b6e64963403945f
godnamra <38734618+godnamra@users.noreply.github.com>
2018-04-25 12:01:30 -0700

    B

---
+++ sfml_project/sfml.2/sfml.2/main.cpp
@@ -0,0 +1,69 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Coordinates
+{
+public:
+	Coordinates(int x1 = 0, int x2 = 0)
+	{}
+	void setxy(int x, int y);
+	int getX();
+	int getY();
+private:
+	int x;
+	int y;
+};
+
+int setxy(int x, int y)
+{
+
+}
+
+int Coordinates::getX()
+{
+	return x;
+}
+
+int Coordinates::getY()
+{
+	return y;
+}
+
+void Coordinates::setxy(int x1, int y1)
+
+class Square : public RectangleShape
+{
+public:
+	Square(Vector2f &pos, Color &c, Vector2f &dim)
+	{
+		setPosition(pos);
+		setFillColor(c);
+		setSize(dim);
+	}
+	char getCheckerContent();
+private:
+	char contains;
+};
+
+char Square::getCheckerContent()
+{
+	return contains;
+}
+
+class Board
+{
+public:
+	char getSquareContent(int x, int y);
+private:
+	Square squares[8][8];
+};
+
+char Board::getSquareContent(int x, int y)
+{
+	return squares[y][x].getCheckerContent();
+}
+
+int main(void)
+{
+
+}
